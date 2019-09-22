fe088548c780a7029167e8bc0658a2b53cdf555c
secisland <secisland@163.com>
2019-09-22 23:08:56 +0800

    Update upsops.lua

--- ngx_lua/upsops.lua
+++ ngx_lua/upsops.lua
@@ -1,12 +1,4 @@
 local cjson = require "cjson";
-local ups = ngx.shared.upstreams;
-local servers = {}
-for _,v in pairs(ups:get_keys(0)) do
-   servers[v] = ups:get(v)
-end
-ngx.say(cjson.encode({code="A00001", msg="OK", data=servers}))
-[root@master ngx_lua_ups]# cat upsops.lua
-local cjson = require "cjson";
 local upstream = require "ngx.upstream"
 local ups = ngx.shared.upstreams;
 
